3b1f4cbc55cdb4bb2957f1070fda11d66d2eb3e2
Auliaaa2108 <90377392+Auliaaa2108@users.noreply.github.com>
2022-10-04 16:33:08 +0700

    recyclerview homescreen

--- app/src/main/java/com/example/myapplication/HomeScreenActivity.java
+++ app/src/main/java/com/example/myapplication/HomeScreenActivity.java
@@ -1,18 +1,71 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.myapplication.adapter.jadwalAdapter;
+import com.example.myapplication.models.Logbook;
+import com.example.myapplication.models.jadwal;
+
+import java.util.ArrayList;
 
 public class HomeScreenActivity extends AppCompatActivity {
+
+    private RecyclerView rvsidsem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        rvsidsem =findViewById(R.id.rvsemsid);
+
+        jadwalAdapter adapter = new jadwalAdapter(getjadwal());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+        rvsidsem.setLayoutManager(layoutManager);
+        rvsidsem.setAdapter(adapter);
+
     }
+
+    public ArrayList<jadwal>getjadwal(){
+
+        ArrayList<jadwal>listjadwal = new ArrayList<>();
+        listjadwal.add(new jadwal(
+                "Aulia Dwi Shaviqa Pohan",
+                0,
+                "11.00 WIB",
+                "Ruang Sidang SI",
+                "Senin, 23 Agustus 2023"
+
+        ));
+        listjadwal.add(new jadwal(
+                "Aulia Dwi Shaviqa Pohan",
+                1,
+                "09.00 WIB",
+                "Ruang Sidang SI",
+                "Rabu, 01 Agustus 2023"
+
+        ));
+        listjadwal.add(new jadwal(
+                "Aulia Dwi Shaviqa Pohan",
+                0,
+                "09.00 WIB",
+                "Ruang Sidang SI",
+                "Rabu, 01 Agustus 2023"
+
+        ));
+        return listjadwal;
+    }
+
+
+
 
     public void profil(View view) {
         Intent intent = new Intent(HomeScreenActivity.this, ProfileActivity.class);
@@ -24,23 +77,4 @@ public class HomeScreenActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void semhas(View view) {
-        Intent intent = new Intent(HomeScreenActivity.this, Semhas_Awal.class);
-        startActivity(intent);
-    }
-
-    public void sidang(View view) {
-        Intent intent = new Intent(HomeScreenActivity.this, Sidang_Awal.class);
-        startActivity(intent);
-    }
-
-    public void detail(View view) {
-        Intent intent = new Intent(HomeScreenActivity.this, Logbook_Detail.class);
-        startActivity(intent);
-    }
-
-    public void add(View view) {
-        Intent intent = new Intent(HomeScreenActivity.this, Logbook_Add.class);
-        startActivity(intent);
-    }
 }
---
+++ app/src/main/java/com/example/myapplication/models/Logbook.java
@@ -0,0 +1,29 @@
+package com.example.myapplication.models;
+
+public class Logbook {
+    String tanggal;
+    int keterangan;
+
+    public Logbook(){}
+
+    public Logbook(String tanggal, int keterangan) {
+        this.tanggal = tanggal;
+        this.keterangan = keterangan;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public int getKeterangan() {
+        return keterangan;
+    }
+
+    public void setKeterangan(int keterangan) {
+        this.keterangan = keterangan;
+    }
+}
