9089c1c6925edbb9e548819233714f0577568f30
Dina1003 <dinalatief.padanglua@gmail.com>
2022-10-14 15:01:52 +0700

    Semhas rv

--- app/src/main/java/com/example/myapplication/adapter/AgendaAdapter.java
+++ app/src/main/java/com/example/myapplication/adapter/AgendaAdapter.java
@@ -63,6 +63,7 @@ public class AgendaAdapter extends RecyclerView.Adapter<AgendaAdapter.AgendaView
     @Override
     public int getItemCount() {
         return  listAgenda.size();
+        //
 
     }
 
