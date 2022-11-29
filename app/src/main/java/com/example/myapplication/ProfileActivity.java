4f6899c64d78d6fafd6633acd83fc20f42472a72
Auliaaa2108 <90377392+Auliaaa2108@users.noreply.github.com>
2022-11-29 10:31:43 +0700

    notif ganti passsword

--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -1,18 +1,68 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private static final String CHANNEL_ID = "Text Channel";
+    private ImageButton gantipass;
+    private NotificationManagerCompat notificationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+        createNotificationChannel();
+
+        gantipass = findViewById(R.id.gantipass);
+        gantipass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent resultIntent = new Intent(ProfileActivity.this, EditProfileActivity.class);
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(ProfileActivity.this);
+                stackBuilder.addNextIntentWithParentStack(resultIntent);
+                PendingIntent resultPendingIntent =
+                        stackBuilder.getPendingIntent(0,
+                                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(ProfileActivity.this,CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_notifikasi)
+                        .setContentTitle("Perubahan Password")
+                        .setContentText("tindakn Perubahan Password, apakah itu anda? tekan  untuk melanjutkan")
+                        .addAction(R.drawable.ic_notifikasi, "Selengkapnya", resultPendingIntent)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                notificationManager.notify(101,builder.build());
+            }
+        });
     }
+    private void createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Permintaan ganti password", importance);
+            channel.setDescription("Anda mencoba mengubah password");
+            notificationManager.createNotificationChannel(channel);
+
+        }
+    }
+
 
     public void home(View view) {
         Intent intent = new Intent(ProfileActivity.this, HomeScreenActivity.class);
