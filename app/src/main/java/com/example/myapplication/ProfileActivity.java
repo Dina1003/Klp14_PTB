40d0bda9c1c95f78c69d525b7b08c48bc46beb6e
Auliaaa2108 <90377392+Auliaaa2108@users.noreply.github.com>
2022-12-23 15:03:10 +0700

    profile done

--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -5,32 +5,61 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import android.annotation.SuppressLint;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.myapplication.datamodel.ProfilResponse;
+import com.example.myapplication.retrofit.StoryClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView name, nim2;
+    ImageView profile;
+    String token;
+
 
     private static final String TAG = "ProfileActivity-Debug";
     private static final String CHANNEL_ID = "Text Channel";
     private ImageButton gantipass;
     private NotificationManagerCompat notificationManager;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        SharedPreferences sharedPref = getSharedPreferences("prefs", Context.MODE_PRIVATE);
+        token = sharedPref.getString("TOKEN","");
+
+        getProfile();
+
+//        profile = findViewById (R.id.profil);
 
         FirebaseMessaging.getInstance().getToken()
                 .addOnCompleteListener(new OnCompleteListener<String>() {
@@ -67,19 +96,64 @@ public class ProfileActivity extends AppCompatActivity {
                                 PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
 
 
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(ProfileActivity.this,CHANNEL_ID)
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(ProfileActivity.this, CHANNEL_ID)
                         .setSmallIcon(R.drawable.ic_notifikasi)
                         .setContentTitle("Perubahan Password")
                         .setContentText("tindakn Perubahan Password, apakah itu anda? tekan  untuk melanjutkan")
                         .addAction(R.drawable.ic_notifikasi, "Selengkapnya", resultPendingIntent)
                         .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-                notificationManager.notify(101,builder.build());
+                notificationManager.notify(101, builder.build());
             }
         });
     }
+
+    private void getProfile() {
+        String API_BASE_URL = "http://ptb-api.husnilkamil.my.id";
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(new OkHttpClient.Builder().addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)).build())
+                .build();
+
+        StoryClient client = retrofit.create(StoryClient.class);
+
+
+        Call<ProfilResponse> call = client.profill("Bearer " + token );
+        call.enqueue(new Callback<ProfilResponse>() {
+            @Override
+            public void onResponse(Call<ProfilResponse> call, Response<ProfilResponse> response) {
+                if (!response.isSuccessful()) {
+                    name.setText("Nama Mahasiswa");
+                    nim2.setText("Nim Mahasiswa");
+                } else {
+                    ProfilResponse user = response.body();
+
+//                    Log.d("YYPROFILEACTIVITY", "Hasilnya: " + response.body().getEmail());
+                    String nama = user.getName();
+                    String nim = user.getUsername();
+
+                    TextView castName = findViewById(R.id.name);
+                    TextView castNim = findViewById(R.id.nim2);
+
+                    castName.setText(nama);
+                    castNim.setText(nim);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ProfilResponse> call, Throwable t) {
+                Toast.makeText(ProfileActivity.this, t.getMessage().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void cekprofile() {
+    }
+
     private void createNotificationChannel() {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
             int importance = NotificationManager.IMPORTANCE_HIGH;
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Permintaan ganti password", importance);
