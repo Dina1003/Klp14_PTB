56ebb3c2750d298998a695fc533c5fa52d7e88b0
Auliaaa2108 <90377392+Auliaaa2108@users.noreply.github.com>
2022-12-23 16:23:25 +0700

    profile revisi

--- app/src/main/java/com/example/myapplication/HomeScreenActivity.java
+++ app/src/main/java/com/example/myapplication/HomeScreenActivity.java
@@ -4,26 +4,81 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myapplication.adapter.AgendaAdapter1;
+import com.example.myapplication.datamodel.ProfilResponse;
 import com.example.myapplication.models.Agenda1;
+import com.example.myapplication.retrofit.StoryClient;
 
 import java.util.ArrayList;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class HomeScreenActivity extends AppCompatActivity implements AgendaAdapter1.itemAgenda1ClickListener{
 
-
+    TextView username;
+    String token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+        SharedPreferences sharedPref = getSharedPreferences("prefs", Context.MODE_PRIVATE);
+        token = sharedPref.getString("TOKEN","");
+
     }
 
+    private void getUsername() {
+        String API_BASE_URL = "http://ptb-api.husnilkamil.my.id";
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(new OkHttpClient.Builder().addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)).build())
+                .build();
+
+        StoryClient client = retrofit.create(StoryClient.class);
+
+
+        Call<ProfilResponse> call = client.profill("Bearer " + token );
+        call.enqueue(new Callback<ProfilResponse>() {
+            @Override
+            public void onResponse(Call<ProfilResponse> call, Response<ProfilResponse> response) {
+                if (!response.isSuccessful()) {
+                    username.setText("Nama Mahasiswa");
+                } else {
+                    ProfilResponse user = response.body();
+
+//                    Log.d("YYPROFILEACTIVITY", "Hasilnya: " + response.body().getEmail());
+                    String nama = user.getName();
+
+                    TextView castName = findViewById(R.id.username);
+
+
+                    castName.setText(nama);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ProfilResponse> call, Throwable t) {
+                Toast.makeText(HomeScreenActivity.this, t.getMessage().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
     public void profil(View view) {
         Intent intent = new Intent(HomeScreenActivity.this, ProfileActivity.class);
